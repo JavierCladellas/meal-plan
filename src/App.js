ff2a66cb64ed203ae21294fa73851e9408b460d4
JavierCladellas <javiercladellas@gmail.com>
2023-10-08 12:31:42 +0200

    cahnged to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import React from 'react';
 import MealPlan from './components/MealPlan';
 import Recipes from './components/Recipes';
