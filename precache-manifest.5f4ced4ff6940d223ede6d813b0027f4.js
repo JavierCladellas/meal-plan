da50945a4dbe8d2ab0e8a60aef6b0db638c61cc7
JavierCladellas <javiercladellas@gmail.com>
2023-10-08 12:22:41 +0200

    Updates

---
+++ precache-manifest.5f4ced4ff6940d223ede6d813b0027f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "119cf6eccac13e530ab0c0e7183a19ae",
+    "url": "%7Bjaviercladellas%7D.github.io/%7Bmeal-plan%7D/index.html"
+  },
+  {
+    "revision": "5674377fffe5a4a3e040",
+    "url": "%7Bjaviercladellas%7D.github.io/%7Bmeal-plan%7D/static/css/main.d02784e3.chunk.css"
+  },
+  {
+    "revision": "dfbf87fa828f4d3ec33e",
+    "url": "%7Bjaviercladellas%7D.github.io/%7Bmeal-plan%7D/static/js/2.03af8f57.chunk.js"
+  },
+  {
+    "revision": "5674377fffe5a4a3e040",
+    "url": "%7Bjaviercladellas%7D.github.io/%7Bmeal-plan%7D/static/js/main.a149f23b.chunk.js"
+  },
+  {
+    "revision": "223eadf512952dc2f040",
+    "url": "%7Bjaviercladellas%7D.github.io/%7Bmeal-plan%7D/static/js/runtime~main.1f39f3c4.js"
+  }
+]);
